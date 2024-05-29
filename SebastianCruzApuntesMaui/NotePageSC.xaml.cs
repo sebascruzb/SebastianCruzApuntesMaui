73c4f5447823b38eb71a7cdee1be791bc592c463
Sebastian Cruz <raul.cruz@udla.edu.ec>
2024-05-29 12:01:10 -0500

    NotePage

--- SebastianCruzApuntesMaui/NotePageSC.xaml.cs
+++ SebastianCruzApuntesMaui/NotePageSC.xaml.cs
@@ -2,8 +2,28 @@ namespace SebastianCruzApuntesMaui;
 
 public partial class NotePageSC : ContentPage
 {
-	public NotePageSC()
-	{
-		InitializeComponent();
-	}
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePageSC()
+    {
+        InitializeComponent();
+
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
 }
